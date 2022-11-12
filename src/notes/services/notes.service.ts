006a16c29b26d77e4c11db4df99ec48db2d7c1d0
Javier Portella <juan.portella1@unmsm.edu.pe>
2022-11-12 07:31:18 -0500

    feat(test): Add test for note module

--- src/notes/services/notes.service.ts
+++ src/notes/services/notes.service.ts
@@ -38,7 +38,7 @@ export class NotesService {
    * @returns The new note that was created.
    */
   async create(body: NoteBodyDto): Promise<Note> {
-    const newNote = this.notesRepository.create({
+    const newNote = await this.notesRepository.create({
       title: body.title,
       description: body.description,
     });
