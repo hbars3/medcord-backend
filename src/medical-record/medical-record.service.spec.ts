792b19e5dd58329fe2307151fdf0beae2fcd45b1
Javier Portella <juan.portella1@unmsm.edu.pe>
2022-12-17 07:22:27 -0500

    feat(test): Add medical record test

--- src/medical-record/medical-record.service.spec.ts
+++ src/medical-record/medical-record.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { MedicalRecordService } from './medical-record.service';
+import { MedicalRecord } from './entities/medicalRecord.entity';
+import { MedicalRecordRegisterDto } from './dto/medicalRecordRegisterDto.dto';
 
 describe('MedicalRecordService', () => {
-  let service: MedicalRecordService;
+  let medicalRecordService: MedicalRecordService;
+  let medicalRecordRepository: MockRepository<MedicalRecord>;
 
+  const mockRepository = () => ({
+    create: jest.fn(),
+    save: jest.fn(),
+  });
+  type MockRepository<MedicalRecord> = Partial<
+    Record<keyof Repository<MedicalRecord>, jest.Mock>
+  >;
+
+  const medicalRecord = new MedicalRecord();
+  const medicalRecordRegisterDto = new MedicalRecordRegisterDto();
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MedicalRecordService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        MedicalRecordService,
+        {
+          provide: getRepositoryToken(MedicalRecord),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
-    service = module.get<MedicalRecordService>(MedicalRecordService);
+    medicalRecordService = moduleRef.get<MedicalRecordService>(MedicalRecordService);
+    medicalRecordRepository = moduleRef.get(getRepositoryToken(MedicalRecord));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should be defined', () => {
+    expect(medicalRecordService).toBeDefined();
+  });
+  
+  it('Should return that it registered the user', async () => {
+    medicalRecordRepository.create.mockReturnValue(medicalRecord);
+    medicalRecordRepository.save.mockReturnThis();
+    expect(await medicalRecordService.create(medicalRecordRegisterDto)).toEqual({
+      medicalRecord: medicalRecord,
+    });
+    expect(medicalRecordRepository.create).toBeCalledTimes(1);
+    expect(medicalRecordRepository.save).toBeCalledTimes(1);
   });
 });
